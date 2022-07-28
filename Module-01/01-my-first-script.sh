2ef0577c34b8496a54fcffb1d39faebd35f474a4
johnbimbo12 <johnbimbo12@gmail.com>
2022-07-28 09:51:00 +0300

    Initial commit

---
+++ Module-01/01-my-first-script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Hello world"
+
+echo "Welcome to the bash shell scripting course"
+
+COURSE="Bash"
+COURSE="Bash Shell Scripting"
+echo "I am going to complete the ${COURSE} course"
